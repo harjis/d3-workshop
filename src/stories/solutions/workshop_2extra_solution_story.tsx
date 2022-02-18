4b9f75e1009e8ee778932e644f59ebf9b15bd6a1
Joonas Harjukallio <harjukallio@gmail.com>
2022-02-18 17:42:25 +0200

    2extra

--- src/stories/solutions/workshop_2extra_solution_story.tsx
+++ src/stories/solutions/workshop_2extra_solution_story.tsx
@@ -1,76 +1,111 @@
-// @ts-expect-error shut the fuck up, Flow!
 import React, { useState } from "react";
 import { storiesOf } from "@storybook/react";
-import { scaleOrdinal } from "d3";
+import { ScaleOrdinal, scaleOrdinal } from "d3";
+
 const config = {
-  red: '#D64242',
-  orange: '#F1C664',
-  yellow: '#F4EB72',
-  green: '#65C092',
-  blue: '#4096C6',
-  lightBlue: '#81C7EF',
-  purple: '#D99BBE',
-  black: '#414141'
+  red: "#D64242",
+  orange: "#F1C664",
+  yellow: "#F4EB72",
+  green: "#65C092",
+  blue: "#4096C6",
+  lightBlue: "#81C7EF",
+  purple: "#D99BBE",
+  black: "#414141",
 };
 
 /**
  * You can find tests for the color bucket here
  * https://github.com/manko/processor-edge/blob/487e97a6212cc84e8c4cd0856793be46756b6e8e/frontend/test/unit/utils/color_test.js
  * */
-storiesOf('Development/d3-workshop/Solutions/2extra', module).addDecorator(getStory => {
-  const style = {
-    background: 'lightgrey',
-    height: window.innerHeight,
-    width: window.innerWidth,
-    overflow: 'scroll'
-  };
-  return <div style={style}>{getStory()}</div>;
-}).add('Plot per row checkboxes', () => {
-  return <Boxes boxes={['1', '2', '3', '4', '5', '6']} />;
-});
+storiesOf("Development/d3-workshop/Solutions/2extra", module)
+  .addDecorator((getStory) => {
+    const style = {
+      background: "lightgrey",
+      height: window.innerHeight,
+      width: window.innerWidth,
+      overflow: "scroll",
+    };
+    return <div style={style}>{getStory()}</div>;
+  })
+  .add("Plot per row checkboxes", () => {
+    return <Boxes boxes={["1", "2", "3", "4", "5", "6"]} />;
+  });
 type Props = {
   boxes: string[];
 };
 const colorBucket = initColorBucket();
 
 function Boxes(props: Props) {
-  const [selectedBoxes, setBoxes] = useState([]);
+  const [selectedBoxes, setBoxes] = useState<string[]>([]);
 
-  const toggleBox = box => setBoxes(selectedBoxes.includes(box) ? [...new Set(selectedBoxes.filter(selectedBox => selectedBox !== box))] : [...new Set(selectedBoxes.concat(box))]);
+  const toggleBox = (box: string) =>
+    setBoxes(
+      selectedBoxes.includes(box)
+        ? [
+            ...new Set(
+              selectedBoxes.filter((selectedBox) => selectedBox !== box)
+            ),
+          ]
+        : [...new Set(selectedBoxes.concat(box))]
+    );
 
-  return <div style={containerStyles()}>
-      {props.boxes.map(box => {
-      return <div key={box} style={{ ...boxStyle(),
-        background: colorBucket(selectedBoxes)(box)
-      }} onClick={() => toggleBox(box)}>
+  return (
+    <div style={containerStyles()}>
+      {props.boxes.map((box) => {
+        return (
+          <div
+            key={box}
+            style={{
+              ...boxStyle(),
+              background: colorBucket(selectedBoxes)(box),
+            }}
+            onClick={() => toggleBox(box)}
+          >
             &nbsp;
-          </div>;
-    })}
-    </div>;
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-function getColorScale(domain: string[]): (...args: Array<any>) => any {
-  const colors = [config.red, config.orange, config.yellow, config.green, config.blue, config.lightBlue, config.purple, config.black];
-  return scaleOrdinal().range(colors).domain(domain).unknown('white');
+function getColorScale(domain: string[]): ScaleOrdinal<string, string, string> {
+  const colors = [
+    config.red,
+    config.orange,
+    config.yellow,
+    config.green,
+    config.blue,
+    config.lightBlue,
+    config.purple,
+    config.black,
+  ];
+  return scaleOrdinal<string, string, string>()
+    .range(colors)
+    .domain(domain)
+    .unknown("white");
 }
 
 function initColorBucket() {
-  let currentIds = [];
+  let currentIds: string[] = [];
   let notFoundIndex = 0;
 
-  const removeNotExisting = (currentIds, newIds) => currentIds.map(currentId => {
-    if (newIds.findIndex(id => currentId === id) === -1) {
-      return `nf${notFoundIndex++}`;
-    } else {
-      return currentId;
-    }
-  });
+  const removeNotExisting = (currentIds: string[], newIds: string[]) =>
+    currentIds.map((currentId) => {
+      if (newIds.findIndex((id) => currentId === id) === -1) {
+        return `nf${notFoundIndex++}`;
+      } else {
+        return currentId;
+      }
+    });
 
-  const addToFirstAvailable = (currentIds, newIds) => {
+  const addToFirstAvailable = (currentIds: string[], newIds: string[]) => {
     const _currentIds = [...currentIds];
-    newIds.forEach(newId => {
-      if (_currentIds.findIndex(id => newId === id) === -1) {
-        const firstAvailableIndex = _currentIds.findIndex(id => id.toString().includes('nf'));
+    newIds.forEach((newId) => {
+      if (_currentIds.findIndex((id) => newId === id) === -1) {
+        const firstAvailableIndex = _currentIds.findIndex((id) =>
+          id.toString().includes("nf")
+        );
 
         if (firstAvailableIndex === -1) {
           _currentIds.push(newId);
@@ -82,10 +117,10 @@ function initColorBucket() {
     return _currentIds;
   };
 
-  return function (newIds) {
+  return function (newIds: string[]) {
     currentIds = removeNotExisting(currentIds, newIds);
     currentIds = addToFirstAvailable(currentIds, newIds);
-    return function (id) {
+    return function (id: string) {
       return getColorScale(currentIds)(id);
     };
   };
@@ -93,16 +128,16 @@ function initColorBucket() {
 
 function containerStyles() {
   return {
-    display: 'flex',
-    height: '100px',
-    width: '600px'
+    display: "flex",
+    height: "100px",
+    width: "600px",
   };
 }
 
 function boxStyle() {
   return {
-    height: '100%',
+    height: "100%",
     flexGrow: 1,
-    border: '1px solid'
+    border: "1px solid",
   };
 }
--- src/stories/workshop_2extra_story.tsx
+++ src/stories/workshop_2extra_story.tsx
@@ -1,55 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import { storiesOf } from "@storybook/react";
+import { ScaleOrdinal, scaleOrdinal } from "d3";
+
 const config = {
-  red: '#D64242',
-  orange: '#F1C664',
-  yellow: '#F4EB72',
-  green: '#65C092',
-  blue: '#4096C6',
-  lightBlue: '#81C7EF',
-  purple: '#D99BBE',
-  black: '#414141'
+  red: "#D64242",
+  orange: "#F1C664",
+  yellow: "#F4EB72",
+  green: "#65C092",
+  blue: "#4096C6",
+  lightBlue: "#81C7EF",
+  purple: "#D99BBE",
+  black: "#414141",
 };
-storiesOf('Development/d3-workshop/2extra', module).addDecorator(getStory => {
-  const style = {
-    background: 'lightgrey',
-    height: window.innerHeight,
-    width: window.innerWidth,
-    overflow: 'scroll'
-  };
-  return <div style={style}>{getStory()}</div>;
-}).add('Plot per row checkboxes', () => {
-  return <Boxes boxes={['1', '2', '3', '4', '5', '6']} />;
-});
-const colors = [config.red, config.orange, config.yellow, config.green, config.blue, config.lightBlue, config.purple, config.black];
+
+/**
+ * You can find tests for the color bucket here
+ * https://github.com/manko/processor-edge/blob/487e97a6212cc84e8c4cd0856793be46756b6e8e/frontend/test/unit/utils/color_test.js
+ * */
+storiesOf("Development/d3-workshop/2extra", module)
+  .addDecorator((getStory) => {
+    const style = {
+      background: "lightgrey",
+      height: window.innerHeight,
+      width: window.innerWidth,
+      overflow: "scroll",
+    };
+    return <div style={style}>{getStory()}</div>;
+  })
+  .add("Plot per row checkboxes", () => {
+    return <Boxes boxes={["1", "2", "3", "4", "5", "6"]} />;
+  });
 type Props = {
   boxes: string[];
 };
 
 function Boxes(props: Props) {
-  return <div style={containerStyles()}>
-      {props.boxes.map(box => {
-      return <div key={box} style={{ ...boxStyle(),
-        background: 'white'
-      }} onClick={() => console.log('Should toggle color')}>
+  const [selectedBoxes, setBoxes] = useState<string[]>([]);
+
+  const toggleBox = (box: string) => console.log("should toggle box");
+
+  return (
+    <div style={containerStyles()}>
+      {props.boxes.map((box) => {
+        return (
+          <div
+            key={box}
+            style={{
+              ...boxStyle(),
+              // background: colorBucket(selectedBoxes)(box),
+              background: "white",
+            }}
+            onClick={() => toggleBox(box)}
+          >
             &nbsp;
-          </div>;
-    })}
-    </div>;
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 function containerStyles() {
   return {
-    display: 'flex',
-    height: '100px',
-    width: '600px'
+    display: "flex",
+    height: "100px",
+    width: "600px",
   };
 }
 
 function boxStyle() {
   return {
-    height: '100%',
+    height: "100%",
     flexGrow: 1,
-    border: '1px solid'
+    border: "1px solid",
   };
 }
