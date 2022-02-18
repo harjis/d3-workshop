6b8c804708968eb3dc897940191e429c59cabc3c
Joonas Harjukallio <harjukallio@gmail.com>
2022-02-18 17:25:55 +0200

    2c

--- src/stories/solutions/workshop_2c_solutions_story.tsx
+++ src/stories/solutions/workshop_2c_solutions_story.tsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { storiesOf } from "@storybook/react";
 import { extent, select, scaleLinear } from "d3";
-storiesOf('Development/d3-workshop/Solutions/2c', module).addDecorator(getStory => {
-  const style = {
-    background: 'lightgrey',
-    height: window.innerHeight,
-    width: window.innerWidth,
-    overflow: 'scroll'
-  };
-  return <div style={style}>{getStory()}</div>;
-}).add('Linear scale with margins', () => {
-  return <LinearScale xMargin={8} yMargin={8} width={200} height={100} points={[0, 5, 10]} />;
-});
+
+storiesOf("Development/d3-workshop/Solutions/2c", module)
+  .addDecorator((getStory) => {
+    const style = {
+      background: "lightgrey",
+      height: window.innerHeight,
+      width: window.innerWidth,
+      overflow: "scroll",
+    };
+    return <div style={style}>{getStory()}</div>;
+  })
+  .add("Linear scale with margins", () => {
+    return (
+      <LinearScale
+        xMargin={8}
+        yMargin={8}
+        width={200}
+        height={100}
+        points={[0, 5, 10]}
+      />
+    );
+  });
 type Props = {
   height: number;
   points: number[];
@@ -20,37 +31,51 @@ type Props = {
   yMargin: number;
 };
 
-class LinearScale extends React.Component<Props> {
-  componentDidMount() {
-    draw(this.ref, this.subtractMargins(this.props));
+function draw(element: SVGSVGElement | null, props: Props): void {
+  if (!element) {
+    return;
   }
 
-  componentDidUpdate(nextProps: Props) {
-    draw(this.ref, this.subtractMargins(nextProps));
-  }
-
-  subtractMargins(props: Props) {
-    return { ...props,
-      height: props.height - props.xMargin * 2,
-      width: props.width - props.yMargin * 2
-    };
-  }
-
-  applyRef = ref => this.ref = ref;
-  ref: Element | null | undefined;
-
-  render() {
-    return <svg height={this.props.height} width={this.props.width}>
-        <g transform={`translate(${this.props.xMargin},${this.props.yMargin})`} ref={this.applyRef} />
-      </svg>;
-  }
-
+  const points = select<SVGSVGElement, number[]>(element)
+    .selectAll("circle")
+    .data(props.points);
+  const xScale = scaleLinear()
+    .range([0, props.width])
+    .domain(extent(props.points) as [number, number]);
+  points
+    .enter()
+    .append("circle")
+    .attr("fill", "black")
+    .attr("r", 2)
+    .attr("cy", props.height / 2)
+    .attr("cx", (d) => xScale(d));
+  const texts = select(element).selectAll("text").data(props.points);
+  texts
+    .enter()
+    .append("text")
+    .attr("text-anchor", "middle")
+    .attr("y", props.height / 2 - 8)
+    .attr("x", (d) => xScale(d))
+    .text((d) => d);
 }
 
-function draw(element: Element | null | undefined, props: Props) {
-  const points = select(element).selectAll('circle').data(props.points);
-  const xScale = scaleLinear().range([0, props.width]).domain(extent(props.points));
-  points.enter().append('circle').attr('fill', 'black').attr('r', 2).attr('cy', props.height / 2).attr('cx', d => xScale(d));
-  const texts = select(element).selectAll('text').data(props.points);
-  texts.enter().append('text').attr('text-anchor', 'middle').attr('y', props.height / 2 - 8).attr('x', d => xScale(d)).text(d => d);
+function subtractMargins(props: Props) {
+  return {
+    ...props,
+    height: props.height - props.xMargin * 2,
+    width: props.width - props.yMargin * 2,
+  };
 }
+const LinearScale = (props: Props): JSX.Element => {
+  const ref = useRef<SVGSVGElement>(null);
+
+  useEffect(() => {
+    draw(ref.current, subtractMargins(props));
+  }, [props]);
+
+  return (
+    <svg height={props.height} width={props.width}>
+      <g transform={`translate(${props.xMargin},${props.yMargin})`} ref={ref} />
+    </svg>
+  );
+};
--- src/stories/workshop_2c_story.tsx
+++ src/stories/workshop_2c_story.tsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { storiesOf } from "@storybook/react";
-storiesOf('Development/d3-workshop/2c', module).addDecorator(getStory => {
-  const style = {
-    background: 'lightgrey',
-    height: window.innerHeight,
-    width: window.innerWidth,
-    overflow: 'scroll'
-  };
-  return <div style={style}>{getStory()}</div>;
-}).add('Linear scale with margins', () => {
-  return <LinearScale xMargin={8} yMargin={8} width={200} height={100} points={[0, 5, 10]} />;
-});
+import { extent, select, scaleLinear } from "d3";
+
+storiesOf("Development/d3-workshop/2c", module)
+  .addDecorator((getStory) => {
+    const style = {
+      background: "lightgrey",
+      height: window.innerHeight,
+      width: window.innerWidth,
+      overflow: "scroll",
+    };
+    return <div style={style}>{getStory()}</div>;
+  })
+  .add("Linear scale with margins", () => {
+    return (
+      <LinearScale
+        xMargin={8}
+        yMargin={8}
+        width={200}
+        height={100}
+        points={[0, 5, 10]}
+      />
+    );
+  });
+
 type Props = {
   height: number;
   points: number[];
@@ -19,22 +32,25 @@ type Props = {
   yMargin: number;
 };
 
-class LinearScale extends React.Component<Props> {
-  componentDidMount() {
-    draw(this.ref, this.props);
-  }
+function draw(element: SVGSVGElement | null, props: Props): void {}
 
-  componentDidUpdate(nextProps: Props) {
-    draw(this.ref, nextProps);
-  }
-
-  applyRef = ref => this.ref = ref;
-  ref: Element | null | undefined;
-
-  render() {
-    return <svg height={this.props.height} width={this.props.width} ref={this.applyRef} />;
-  }
-
+function subtractMargins(props: Props) {
+  return {
+    ...props,
+    height: props.height - props.xMargin * 2,
+    width: props.width - props.yMargin * 2,
+  };
 }
+const LinearScale = (props: Props): JSX.Element => {
+  const ref = useRef<SVGSVGElement>(null);
 
-function draw(element: Element | null | undefined, props: Props) {}
+  useEffect(() => {
+    draw(ref.current, subtractMargins(props));
+  }, [props]);
+
+  return (
+    <svg height={props.height} width={props.width}>
+      <g transform={`translate(${props.xMargin},${props.yMargin})`} ref={ref} />
+    </svg>
+  );
+};
