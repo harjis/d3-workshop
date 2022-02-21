cc48479a4104737e3fc6dbf2221da730de8a9630
Joonas Harjukallio <harjukallio@gmail.com>
2022-02-21 09:41:41 +0200

    3a

--- src/stories/workshop_3a_story.tsx
+++ src/stories/workshop_3a_story.tsx
@@ -1,44 +1,25 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { storiesOf } from "@storybook/react";
-storiesOf('Development/d3-workshop/3', module).addDecorator(getStory => {
-  const style = {
-    background: 'lightgrey',
-    height: window.innerHeight,
-    width: window.innerWidth,
-    overflow: 'scroll'
-  };
-  return <div style={style}>{getStory()}</div>;
-}).add('BarChart', () => {
-  return <BarChart margin={8} width={500} height={400} bars={[]} />;
-});
+
+storiesOf("Development/d3-workshop/3", module)
+  .addDecorator((getStory) => {
+    const style = {
+      background: "lightgrey",
+      height: window.innerHeight,
+      width: window.innerWidth,
+      overflow: "scroll",
+    };
+    return <div style={style}>{getStory()}</div>;
+  })
+  .add("BarChart", () => {
+    return <BarChart margin={8} width={500} height={400} bars={[]} />;
+  });
 type Props = {
   height: number;
   margin: number;
   bars: any;
   width: number;
 };
-
-class BarChart extends React.Component<Props> {
-  componentDidMount() {
-    drawChart(this.ref, this.props);
-  }
-
-  componentDidUpdate(nextProps: Props) {
-    drawChart(this.ref, nextProps);
-  }
-
-  applyRef = ref => this.ref = ref;
-  ref: Element | null | undefined;
-
-  render() {
-    return <svg height={this.props.height} width={this.props.width} style={{
-      background: 'white'
-    }}>
-        <g ref={this.applyRef} />
-      </svg>;
-  }
-
-}
 
 function drawChart(element: Element | null | undefined, props: Props) {
   drawXAxis();
@@ -51,3 +32,31 @@ function drawXAxis() {}
 function drawYAxis() {}
 
 function drawBars() {}
+
+const BarChart = (props: Props): JSX.Element => {
+  const ref = useRef<SVGSVGElement>(null);
+
+  function subtractMargins(props: Props) {
+    return {
+      ...props,
+      height: props.height - props.margin * 2,
+      width: props.width - props.margin * 2,
+    };
+  }
+
+  useEffect(() => {
+    drawChart(ref.current, subtractMargins(props));
+  }, [props]);
+
+  return (
+    <svg
+      height={props.height}
+      width={props.width}
+      style={{
+        background: "white",
+      }}
+    >
+      <g transform={`translate(${props.margin},${props.margin})`} ref={ref} />
+    </svg>
+  );
+};
